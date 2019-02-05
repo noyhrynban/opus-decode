8cf64d195b81fa85c3eace86a42d65d31a12930e
Ryan Harper <36581272+noyhrynban@users.noreply.github.com>
2019-02-05 14:01:47 -0700

    Parse OGGOpus file into opus packets (#2)
    * Convert the TOC byte into a struct
    
    This new funtion doesn't have testing nor does it actually do what
    we would want from it.
    There is only the information from the TOC config byte in the
    packet so far.
    
    * Remove Options and return OpusPacket wrapped in Result
    
    * Add main.rs
    
    main.rs reads in a .opus audio file and prints the number of packets
    
    * Fix bug in match arm and add testing of toc byte arms
    
    * Remove example test
    
    * Attempt to add data to the Opus Packet
    
    * Fix lifetime problems by just using Vec<u8> rather than slices
    
    * Add clippy to CI for analysis and specify script for Travis CI
    
    * Move tests into separate tests/ directory to be excluded by kcov

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,205 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+// #[derive(Debug)]
+pub enum PacketMode {
+    SILK,   // low bandwidth algorithm
+    Hybrid, // both SILK and CELT
+    CELT,   // high bandwidth algorithm
+}
+
+// Taken from https://tools.ietf.org/html/rfc6716#section-2
+// +----------------------+-----------------+-------------------------+
+// | Abbreviation         | Audio Bandwidth | Sample Rate (Effective) |
+// +----------------------+-----------------+-------------------------+
+// | NB (narrowband)      |           4 kHz |                   8 kHz |
+// |                      |                 |                         |
+// | MB (medium-band)     |           6 kHz |                  12 kHz |
+// |                      |                 |                         |
+// | WB (wideband)        |           8 kHz |                  16 kHz |
+// |                      |                 |                         |
+// | SWB (super-wideband) |          12 kHz |                  24 kHz |
+// |                      |                 |                         |
+// | FB (fullband)        |      20 kHz (*) |                  48 kHz |
+// +----------------------+-----------------+-------------------------+
+// #[derive(Debug)]
+pub enum Bandwidth {
+    Narrow,
+    Medium,
+    Wide,
+    SuperWide,
+    Full,
+}
+
+// #[derive(Debug)]
+pub enum Signal {
+    Mono,
+    Stereo,
+}
+
+// #[derive(Debug)]
+pub enum FrameCountCode {
+    Single,
+    TwoEqual,
+    TwoDifferent,
+    Arbitrary,
+}
+
+// #[derive(Debug)]
+pub struct PacketConfiguration {
+    pub mode: PacketMode,
+    pub bandwidth: Bandwidth,
+    pub frame_size: f32,
+    pub signal: Signal,
+    pub code: FrameCountCode,
+}
+
+pub struct OpusPacket {
+    config: PacketConfiguration,
+    data: Vec<u8>,
+}
+
+pub fn packet_config_from_toc_byte(toc_byte: u8) -> Result<PacketConfiguration, &'static str> {
+    let config_val: u8 = 0b0000_1111 & toc_byte;
+
+    let mode: PacketMode;
+    let bandwidth: Bandwidth;
+    let frame_size: f32;
+    match config_val {
+        0...11 => {
+            mode = PacketMode::SILK;
+            match config_val {
+                0...3 => {
+                    bandwidth = Bandwidth::Narrow;
+                    match config_val {
+                        0 => frame_size = 10.0,
+                        1 => frame_size = 20.0,
+                        2 => frame_size = 40.0,
+                        3 => frame_size = 60.0,
+                        _ => return Err("match_code_byte_failed impossibly"),
+                    }
+                }
+                4...7 => {
+                    bandwidth = Bandwidth::Medium;
+                    match config_val {
+                        4 => frame_size = 10.0,
+                        5 => frame_size = 20.0,
+                        6 => frame_size = 40.0,
+                        7 => frame_size = 60.0,
+                        _ => return Err("match_code_byte_failed impossibly"),
+                    }
+                }
+                8...11 => {
+                    bandwidth = Bandwidth::Wide;
+                    match config_val {
+                        8 => frame_size = 10.0,
+                        9 => frame_size = 20.0,
+                        10 => frame_size = 40.0,
+                        11 => frame_size = 60.0,
+                        _ => return Err("match_code_byte_failed impossibly"),
+                    }
+                }
+                _ => return Err("match_code_byte_failed impossibly"),
+            }
+        }
+        12...15 => {
+            mode = PacketMode::Hybrid;
+            match config_val {
+                12...13 => {
+                    bandwidth = Bandwidth::SuperWide;
+                    match config_val {
+                        12 => frame_size = 10.0,
+                        13 => frame_size = 20.0,
+                        _ => return Err("match_code_byte_failed impossibly"),
+                    }
+                }
+                14...15 => {
+                    bandwidth = Bandwidth::Full;
+                    match config_val {
+                        14 => frame_size = 10.0,
+                        15 => frame_size = 20.0,
+                        _ => return Err("match_code_byte_failed impossibly"),
+                    }
+                }
+                _ => return Err("match_code_byte_failed impossibly"),
+            }
+        }
+        16...31 => {
+            mode = PacketMode::CELT;
+            match config_val {
+                16...19 => {
+                    bandwidth = Bandwidth::Narrow;
+                    match config_val {
+                        16 => frame_size = 2.5,
+                        17 => frame_size = 5.0,
+                        18 => frame_size = 10.0,
+                        19 => frame_size = 20.0,
+                        _ => return Err("match_code_byte_failed impossibly"),
+                    }
+                }
+                20...23 => {
+                    bandwidth = Bandwidth::Wide;
+                    match config_val {
+                        20 => frame_size = 2.5,
+                        21 => frame_size = 5.0,
+                        22 => frame_size = 10.0,
+                        23 => frame_size = 20.0,
+                        _ => return Err("match_code_byte_failed impossibly"),
+                    }
+                }
+                24...27 => {
+                    bandwidth = Bandwidth::SuperWide;
+                    match config_val {
+                        24 => frame_size = 2.5,
+                        25 => frame_size = 5.0,
+                        26 => frame_size = 10.0,
+                        27 => frame_size = 20.0,
+                        _ => return Err("match_code_byte_failed impossibly"),
+                    }
+                }
+                28...31 => {
+                    bandwidth = Bandwidth::Full;
+                    match config_val {
+                        28 => frame_size = 2.5,
+                        29 => frame_size = 5.0,
+                        30 => frame_size = 10.0,
+                        31 => frame_size = 20.0,
+                        _ => return Err("match_code_byte_failed impossibly"),
+                    }
+                }
+                _ => return Err("match_code_byte_failed impossibly"),
+            }
+        }
+        _ => return Err("match_code_byte_failed impossibly"),
+    }
+
+    let signal: Signal = match (toc_byte >> 4) & 0b0001 {
+        0 => Signal::Mono,
+        1 => Signal::Stereo,
+        _ => return Err("match_code_byte_failed impossibly"),
+    };
+
+    let code: FrameCountCode = match toc_byte >> 6 {
+        0 => FrameCountCode::Single,
+        1 => FrameCountCode::TwoEqual,
+        2 => FrameCountCode::TwoDifferent,
+        3 => FrameCountCode::Arbitrary,
+        _ => return Err("match_code_byte_failed impossibly"),
+    };
+
+    Ok(PacketConfiguration {
+        mode,
+        bandwidth,
+        frame_size,
+        signal,
+        code,
+    })
+}
+
+pub fn get_opus_packet(packet_data: Vec<u8>) -> Result<OpusPacket, &'static str> {
+    if let Some((toc_byte, data)) = packet_data.split_first() {
+        Ok(OpusPacket {
+            config: packet_config_from_toc_byte(*toc_byte).unwrap(),
+            data: data.to_vec(),
+        })
+    } else {
+        Err("splitting the packet into a TOC byte and data failed")
     }
 }
---
+++ src/main.rs
@@ -0,0 +1,47 @@
+// The basic structure of this was started from
+// https://github.com/RustAudio/ogg/blob/master/examples/dump-all.rs
+
+extern crate opus_decode;
+
+use ogg::PacketReader;
+use std::env;
+use std::fs::File;
+
+fn main() {
+    match run() {
+        Ok(_) => (),
+        Err(err) => println!("Error: {}", err),
+    }
+}
+
+fn run() -> Result<(), std::io::Error> {
+    let file_path = env::args()
+        .nth(1)
+        .expect("No arg found. Please specify a file to open.");
+    println!("Opening file: {}", file_path);
+    let mut file = File::open(file_path)?;
+    let mut packet_reader = PacketReader::new(&mut file);
+    let _header = packet_reader.read_packet(); // This packet should be verified to be the OpusHeader packet, at most
+    let _comments_header = packet_reader.read_packet(); // This is where the Metadata is stored. Maye we'll parse this out someday.
+
+    let mut counter = 0;
+    loop {
+        let r = packet_reader.read_packet();
+        match r {
+            Ok(Some(ogg_packet)) => {
+                let opus_bytes = ogg_packet.data;
+                // next we will call some funtion that takes the Vec<u8> and returns an OPUS packet struct
+                let _opus_packet = opus_decode::get_opus_packet(opus_bytes);
+            }
+            // End of stream
+            Ok(None) => break,
+            Err(e) => {
+                println!("Encountered Error: {:?}", e);
+                break;
+            }
+        }
+        counter += 1;
+    }
+    println!("Found {} packets.", counter);
+    Ok(())
+}
